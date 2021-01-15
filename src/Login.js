9593da07536a1fe50d1ef192cf5f83e6f67e9de4
Jihyun-Jang <jhjang742@gmail.com>
2021-01-15 23:24:14 +0000

    Make login form

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react';
 
 export default function Login() {
+
+    const nameRef = useRef();
+    const passwordRef = useRef();
+
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>Login page</h1>
-        </div>
+        <form onSubmit={handleSubmit} >
+            <h1>Sign In</h1>
+            <label htmlFor="userName">User Name:</label>
+            <input type="text" id="userName" ref={nameRef} required/>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" ref={passwordRef} required />
+            <button type="submit">Sign in</button>
+        </form>
     )
 }
