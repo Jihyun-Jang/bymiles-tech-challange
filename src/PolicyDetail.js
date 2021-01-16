2f4fbe63d945f3b555123b3c2d3c0317a987722e
Jihyun-Jang <jhjang742@gmail.com>
2021-01-16 00:42:41 +0000

    Fetch policy data

--- src/PolicyDetail.js
+++ src/PolicyDetail.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function PolicyDetail() {
+
+    const token = window.localStorage.getItem("access_token");
+    const [ policyData, setPolicyData ] = useState();
+
+    useEffect(() => {
+        
+        fetch("https://api.bybits.co.uk/policys/details", {
+
+            method: "GET",
+            headers: {
+                "environment": "mock",
+                "Authorization": `Bearer ${token}`,
+                "Content-type": "application/json"
+            }        
+        })
+        .then(res => {
+            if(!res.ok) throw new Error(res.status);
+            return res.json();
+        })
+        .then(json => {            
+            setPolicyData(json)
+        })
+        .catch(error => console.error(error))
+
+    }, []);
+
     return (
         <div>
             <h1>My Policy</h1>
-            <p>Policy reference:</p>
+            {/* <p>Policy reference:</p>
+            <p>{}</p>
             <p>Cover type</p>
+            <p>{}</p>
             <p>Car</p>
+            <p>{}</p>
             <p>Address</p>
+            <p>{}</p> */}
         </div>
     )
 }
